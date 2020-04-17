f0bc78a08e548eda40f9f7dcf8d0e0e7862c4135
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-04-17 07:10:56 +0530

    methods of LRU

--- LRU.py
+++ LRU.py
@@ -1,7 +1,13 @@
 class LRU :  
     cache_capacity = 0
     def __init__(self, MAX_CAPACITY) :
-        
+
         self.cache_capacity = MAX_CAPACITY
+
+    def put(self, uri, data) :
+    
+    def get(self, uri) :
+
+    def get_cache(self) :
 
 
