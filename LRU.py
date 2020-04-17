1df07f2ccaa9124f8317d1f3c0ca3adec144eb44
Himaja63 <55688372+Himaja63@users.noreply.github.com>
2020-04-17 07:14:13 +0530

    added main method along with assert statements

--- LRU.py
+++ LRU.py
@@ -11,3 +11,28 @@ class LRU :
     def get_cache(self) :
 
 
+def main() :
+    
+    obj = LRU(5)
+
+    assert (obj.get("1")) == None
+
+    for file in os.listdir() :
+        if file.startswith("file") :
+            data = open(file, "r").read()
+            obj.put(file, data)
+
+    assert (obj.get("file1.txt")) == "This is First file."
+    assert (obj.get("file2.txt")) == "This is Second file."
+    assert (obj.get("file3.txt")) == "This is Third file."
+    assert (obj.get("file4.txt")) == "This is Fourth file."
+    assert (obj.get("file5.txt")) == "This is Fifth file."
+    obj.put("1",2)
+    assert(obj.get("file1.txt")) == None
+    obj.put("2",3)
+    obj.put("3",4)
+    assert(obj.get("1")) == None
+    assert(obj.get("2")) == None
+
+
+    
